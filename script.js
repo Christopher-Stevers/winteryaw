7bf0f1bd610dad72e1b7fdd228483e243a0e5526
Christopher-Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2020-10-10 15:29:22 -0400

    added files to main

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+    function displayList() {
+        document.getElementById("menu").style.visibility = "visible";
+        document.getElementById("icon").style.visibility = "hidden";
+    };
+    function hideList() {
+        document.getElementById("icon").style.visibility = "visible";
+        document.getElementById("menu").style.visibility = "hidden";
+    };
