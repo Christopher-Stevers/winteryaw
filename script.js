3d95bc6533d3c19e03f5224c8c334533cf32c264
Christopher Stevers <christopher.stevers1@gmail>
2020-10-20 07:32:33 -0400

    seperated js from html

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
   
+  $(document).ready(function () {
+        $("#contact-form").submit(function (e) {
+            const form = document.querySelector('form[id="contact-form"] ');
+            const username = form.elements["user_name"].value;
+            const userMail = form.elements["user_email"].value;
+            const subject = form.elements["subject"].value;
+            const message = form.elements["message"].value;
+            e.preventDefault();
+            var data = {
+                service_id: "service_oqnbbzb",
+                template_id: "template_2ogzbrc",
+                user_id: "user_2alctVHDRyRs2gc1ntQhw",
+                template_params: {
+                    from_name: username,
+                    from_email: userMail,
+                    to_name: "Chris",
 
+                    subject: subject,
+                    message: message
+                }
+            };
+            $.ajax("https://api.emailjs.com/api/v1.0/email/send", {
+                type: "POST",
+                data: JSON.stringify(data),
+                contentType: "application/json"
+            }).done(function () {
+                alert("Your mail is sent!");
+            }).fail(function (error) {
+                alert("Oops… " + JSON.stringify(error));
+            })
+        })
+    })
     function displayList() {
         document.getElementById("menu").style.visibility = "visible";
         document.getElementById("icon").style.visibility = "hidden";
